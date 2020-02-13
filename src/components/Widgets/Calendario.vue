7c645f8e98fe87254e24d1160fa326fcf5738112
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2020-02-13 05:05:11 -0400

    Mascara nombre de semana

--- src/components/Widgets/Calendario.vue
+++ src/components/Widgets/Calendario.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="calendario">
-    <div class="col-md-12">
-      <calendar :attributes='attrsCalendar' :disabled-dates='ddCalendar' class='m-0 p-0' style="height:239px;" is-expanded/>
-    </div>
+      <div class="col-md-12 mb-sm-4">
+        <calendar :attributes='attrsCalendar' :disabled-dates='ddCalendar'  :masks="{ weekdays: 'WW' }" class='m-0 p-0' style="height:239px;" is-expanded/>
+      </div>
   </div>
 </template>
 <script>
