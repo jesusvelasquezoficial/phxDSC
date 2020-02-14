5569a86eb003b7fae43fecfa3603172c00853946
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2020-02-14 15:16:23 -0400

    new directorio de monedas

---
+++ src/pages/currencies/petroleo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="petroleo">
+    <page-title-bar></page-title-bar>
+		<crypto-slider></crypto-slider>
+    <b-row>
+      <b-col>
+        <graph-petroleo></graph-petroleo>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+<script>
+export default {
+  name: "petroleo",
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
